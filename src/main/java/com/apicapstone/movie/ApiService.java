332130337361e2bd47efa7d3072e97b98018ada1
Lauren Mattes <laurenmattes@outlook.com>
2019-02-27 15:56:45 -0500

    here ya go

---
+++ src/main/java/com/apicapstone/movie/ApiService.java
@@ -0,0 +1,10 @@
+package com.apicapstone.movie;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ApiService {
+
+	@Value("${9daef3ac188ee8cf078562e2cf5e7709}")
+	String myApiKey;
+
+}
---
+++ src/main/java/com/apicapstone/movie/Movie.java
@@ -0,0 +1,5 @@
+package com.apicapstone.movie;
+
+public class Movie {
+
+}
