1657303ab28e51a7856357ada69174989eee7d45
vell_2x <vell690@yahoo.com>
2019-02-27 17:22:07 -0500

    Completed Movie Object class

--- src/main/java/com/apicapstone/movie/Movie.java
+++ src/main/java/com/apicapstone/movie/Movie.java
@@ -1,5 +1,38 @@
 package com.apicapstone.movie;
 
 public class Movie {
+	private String title;
+	private String rating;
+	private String genre;
+	private Integer length;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public Integer getLength() {
+		return length;
+	}
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+	@Override
+	public String toString() {
+		return "Movie [title=" + title + ", rating=" + rating + ", genre=" + genre + ", length=" + length + "]";
+	}
 
 }
