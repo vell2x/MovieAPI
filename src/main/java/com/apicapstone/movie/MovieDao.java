2912610fe743d245df6bb7d4e7723b00849425f6
Carl Scozzari <carlscozzari@Carls-MacBook-Pro.local>
2019-02-28 09:33:06 -0500

    DAOUpdate

--- src/main/java/com/apicapstone/movie/MovieDao.java
+++ src/main/java/com/apicapstone/movie/MovieDao.java
@@ -1,5 +1,25 @@
 package com.apicapstone.movie;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import co.grandcircus.restserver.entity.Product;
+
+@Repository
+@Transactional
 public class MovieDao {
 
+	@PersistenceContext
+	private EntityManager em;
+	
+	public List<Moive> findAll() {
+		return em.createQuery("from Moive", Movie.class).getResultList();
+	}
+	
+
+	
 }
