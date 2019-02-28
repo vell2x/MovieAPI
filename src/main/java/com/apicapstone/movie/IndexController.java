5cae69f187af22871797766ae417898c0282c1bb
Lauren Mattes <laurenmattes@outlook.com>
2019-02-28 09:27:26 -0500

    indexcontroller

--- src/main/java/com/apicapstone/movie/IndexController.java
+++ src/main/java/com/apicapstone/movie/IndexController.java
@@ -1,5 +1,15 @@
 package com.apicapstone.movie;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class IndexController {
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
 
 }
