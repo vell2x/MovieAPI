0c859bef449c7f2dc17945dba0f64db46127c070
vell_2x <vell690@yahoo.com>
2019-03-01 00:44:21 -0500

    Working on API

--- src/main/java/com/apicapstone/movie/Movie.java
+++ src/main/java/com/apicapstone/movie/Movie.java
@@ -6,14 +6,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Movie {
 	private String title;
-	private String character;
 	@JsonProperty("release_date")
 	private String releaseDate;
-	@JsonProperty("vote_count")
-	private Integer voteCount;
-	@JsonProperty("vote_average")
-	private Double voteAverage;
-	private Integer id;
 	private String overview;
 	public String getTitle() {
 		return title;
@@ -21,35 +15,11 @@ public class Movie {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getCharacter() {
-		return character;
-	}
-	public void setCharacter(String character) {
-		this.character = character;
-	}
 	public String getReleaseDate() {
 		return releaseDate;
 	}
 	public void setReleaseDate(String releaseDate) {
 		this.releaseDate = releaseDate;
-	}
-	public Integer getVoteCount() {
-		return voteCount;
-	}
-	public void setVoteCount(Integer voteCount) {
-		this.voteCount = voteCount;
-	}
-	public Double getVoteAverage() {
-		return voteAverage;
-	}
-	public void setVoteAverage(Double voteAverage) {
-		this.voteAverage = voteAverage;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
 	}
 	public String getOverview() {
 		return overview;
@@ -59,9 +29,6 @@ public class Movie {
 	}
 	@Override
 	public String toString() {
-		return "Movie [title=" + title + ", character=" + character + ", releaseDate=" + releaseDate + ", voteCount="
-				+ voteCount + ", voteAverage=" + voteAverage + ", id=" + id + ", overview=" + overview + "]";
+		return "Movie [title=" + title + ", releaseDate=" + releaseDate + ", overview=" + overview + "]";
 	}
-	
-	
 }
